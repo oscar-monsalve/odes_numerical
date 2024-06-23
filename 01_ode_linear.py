3bdb9046db3590e2318a64517bd3db42ab53fe56
oscar-monsalve <oscarmonsalvec5@gmail.com>
2024-06-23 15:50:48 -0500

    update

--- 01_ode_linear.py
+++ 01_ode_linear.py
@@ -2,25 +2,39 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy as sp
 from scipy.integrate import odeint
 from scipy.integrate import solve_ivp
 
 
 # Define the right side of the ODE when is written is the form "dv/dt = ax^2 + b"
-def dvdt(t: float, v: float, beta: float) -> float:
+def dvdt(t: float, v: float) -> float:
+    beta = 5  # constant
     return 3*v**2 - beta
 
 
-# Define the time range, the initial conditions, and the constant values
+# Define the time range and the initial conditions
 
 t = np.linspace(0, 1, 100)  # time range
-v0 = 0  # Velocity initial condition -> At t = 0, the Velocity is 0
-beta = 5
+v0 = 0  # Velocity initial condition -> At t = 0, v = 0
 
 # Solve the ODE. The are two main solvers in scipy
 # - odeint: Uses a particular solver called Isoda from the FORTRAN library odepack.
 # - solve_ivp: More customizable, can choose from a list of possible solvers.
 
-sol_m1 = odeint()
+sol_m1 = odeint(dvdt, y0=v0, t=t, tfirst=True)
+sol_m2 = solve_ivp(dvdt, t_span=(0, max(t)), y0=[v0], t_eval=t)
 
+v_sol_m1 = sol_m1.T[0]
+v_sol_m2 = sol_m2.y[0]
+
+# print(sol_m1.T[0])
+# print(sol_m2.y[0])
+
+# Plot the solution
+plt.scatter(t, v_sol_m1, s=7, c="r", label="odeint")
+plt.plot(t, v_sol_m2, '--', label="solve_ivp")
+plt.title("ODE solver comparison")
+plt.ylabel(r"$v(t)$")
+plt.xlabel(r"$t$")
+plt.legend()
+plt.show()
--- README.md
+++ README.md
@@ -4,17 +4,17 @@
 
 The example ODE model air friction while falling. The ODE is the following:
 
-$$\displaystyle \frac{dv}{dt} - \alpha v^2 + \beta = 0\\; \mathrm{with initial condition}\\; v(0) = 0$$
+$$\displaystyle \frac{dv}{dt} - \alpha v^2 + \beta = 0\\; \mathrm{with\\;initial\\; condition}\\; v(0) = 0$$
 
 where $v$ is velocity, $t$ is time, and $\alpha,\\, \beta$ are constants.
 
 First, we write the ODE in the following form:
 
-$$\displaystyle \frac{dv}{dt} = f(r\\, v)$$
+$$\displaystyle \frac{dv}{dt} = f(t\\, v)$$
 
 In other words, "the derivative of $v$ equals a function that depends on the velocity $v$ and time $t$". For this case, we can write it as:
 
-$$\displaystyle \frac{dv}{dt} ? \alpha v^2 - \beta$$
+$$\displaystyle \frac{dv}{dt} = \alpha v^2 - \beta$$
 
 
 ## 2. Coupled first order ODE's
